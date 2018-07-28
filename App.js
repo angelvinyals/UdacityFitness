4e3c545da7c87165a105ecf82f14c129261b2504
angelvinyals <angel.vinyals@gmail.com>
2018-07-29 00:31:42 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { View} from 'react-native';
+import {createStore } from 'redux'
+import { Provider } from  'react-redux'
+import reducer from './reducers'
+import History from './components/History'
+
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Provider store= {createStore(reducer)}>          
+        <View style= {{flex:1}}>
+          <View style= {{height:20}}/>
+          <History/>          
+        </View>
+      </Provider>
+    )
+  }
+}
---
+++ utils/helpers.js
@@ -0,0 +1,148 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+//import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { white, red, orange,  blue, lightPurp, pink ,  gold, yellow } from './colors'
+
+export function getDailyReminderValue () {
+	return{
+		today: "Don't forget to log your data today!"
+	}
+}
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20
+  },
+})
+
+
+export function isBetween (num, x, y) {
+  if (num >= x && num <= y) {
+    return true
+  }
+
+  return false
+}
+
+export function calculateDirection (heading) {
+  let direction = ''
+
+  if (isBetween(heading, 0, 22.5)) {
+    direction = 'North'
+  } else if (isBetween(heading, 22.5, 67.5)) {
+    direction = 'North East'
+  } else if (isBetween(heading, 67.5, 112.5)) {
+    direction = 'East'
+  } else if (isBetween(heading, 112.5, 157.5)) {
+    direction = 'South East'
+  } else if (isBetween(heading, 157.5, 202.5)) {
+    direction = 'South'
+  } else if (isBetween(heading, 202.5, 247.5)) {
+    direction = 'South West'
+  } else if (isBetween(heading, 247.5, 292.5)) {
+    direction = 'West'
+  } else if (isBetween(heading, 292.5, 337.5)) {
+    direction = 'North West'
+  } else if (isBetween(heading, 337.5, 360)) {
+    direction = 'North'
+  } else {
+    direction = 'Calculating'
+  }
+
+  return direction
+}
+
+export function timeToString (time = Date.now()) {
+  const date = new Date(time)
+  const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
+  return todayUTC.toISOString().split('T')[0]
+}
+
+
+export function getMetricMetaInfo (metric) {
+	const info ={
+		run:{
+			displayname:'Run',
+			max: 50,
+			unit:'km',
+			step:1,
+			type: 'steppers',
+			getIcon() {
+				return (
+					<View style={[styles.iconContainer, {backgroundColor: red}]}>
+						<Text>icon-run</Text>
+					</View>
+				)
+			}
+		},
+		bike:{
+			displayname:'Bike',
+			max: 100,
+			unit:'km',
+			step:1,
+			type: 'steppers',
+			getIcon() {
+				return (
+					<View style={[styles.iconContainer, {backgroundColor: pink}]}>
+						<Text>icon-bike</Text>
+					</View>
+				)
+			}
+		},
+		swim:{
+			displayname:'Swim',
+			max: 9900,
+			unit:'m',
+			step:100,
+			type: 'steppers',
+			getIcon() {
+				return (
+					<View style={[styles.iconContainer, {backgroundColor: blue}]}>
+						<Text>icon-swim</Text>
+					</View>
+				)
+			}
+		},
+		sleep:{
+			displayname:'Sleep',
+			max: 24,
+			unit:'hours',
+			step:1,
+			type: 'slider',
+			getIcon() {
+				return (
+					<View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
+						<Text>icon-bed</Text>
+					</View>
+				)
+			}
+		},
+		eat:{
+			displayname:'Eat',
+			max: 10,
+			unit:'rating',
+			step:1,
+			type: 'slider',
+			getIcon() {
+				return (
+					<View style={[styles.iconContainer, {backgroundColor: yellow }]}>
+						<Text>icon-eat</Text>
+					</View>
+				)
+			}
+		},
+
+	}
+
+	return typeof metric === 'undefined'
+		? info
+		: info[metric]
+
+}
+
