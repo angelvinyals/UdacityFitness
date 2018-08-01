c83658a75c2857f43576fc307755d3383a7ec2e5
angelvinyals <angel.vinyals@gmail.com>
2018-08-02 01:10:50 +0200

    added AddEntry

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {createStore } from 'redux'
 import { Provider } from  'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import AddEntry from './components/AddEntry'
 
 
 export default class App extends Component<Props> {
@@ -15,7 +16,7 @@ export default class App extends Component<Props> {
       <Provider store= {createStore(reducer)}>          
         <View style= {{flex:1}}>
           <View style= {{height:20}}/>
-            <History/>
+            <AddEntry/>
         </View>
       </Provider>
     )
