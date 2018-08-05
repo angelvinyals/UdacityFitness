e650ca8e325eb73a70239959700d9c20e56bd245
angelvinyals <angel.vinyals@gmail.com>
2018-08-05 19:40:10 +0200

    add Reset button to EntryDetails
    we have to use shouldComponentUpdate  with metrics next props in order to fix rerenders error

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -3,6 +3,12 @@ import { View, Text, StyleSheet } from 'react-native'
 import { connect } from  'react-redux'
 import { white, purple } from  '../utils/colors'
 import MetricCard from './MetricCard'
+import { addEntry } from '../actions'
+import { removeEntry } from '../utils/api'
+import { getDailyReminderValue, timeToString } from '../utils/helpers'
+import TextButton from  './TextButton'
+
+
 
 class EntryDetail extends Component {
 
@@ -23,16 +29,33 @@ class EntryDetail extends Component {
 		    headerTitleStyle: {
 		    	fontWeight: 'bold',
 		    },
-	    };
+	    }
+	}
+
+	reset = () => {
+		
+		const {entryId, remove, goBack} = this.props
+
+		remove()
+		goBack()
+		removeEntry(entryId)
+
+	}
+
+	shouldComponentUpdate ( nextProps ){
+		return nextProps.metrics !== null && !nextProps.metrics.today
 	}
 
  	render(){
- 		const { metrics } = this.props
+ 		const { metrics, navigation } = this.props
+ 		const { entryId } = navigation.state.params
 
 		return(
 			<View style= {styles.container}>
 				<MetricCard metrics={metrics} />
-				<Text>Entry Detail - {this.props.navigation.state.params.entryId}</Text>
+				<TextButton onPress={this.reset} style={styles.resetBtn}>
+					RESET
+				</TextButton>
 			</View>
 		)
 	}
@@ -43,6 +66,19 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: white
+  },
+  resetBtn: {
+  	fontWeight: 'bold',
+  	color:white,
+  	backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
 
@@ -55,6 +91,19 @@ function mapStateToProps (state, { navigation }) {
 	}
 	
 }
-export default connect(mapStateToProps)(EntryDetail)
+
+function mapDispatchToPrpps (dispatch, {navigation}) {
+	const { entryId } = navigation.state.params
+
+	return{
+		remove: ()=> dispatch(addEntry({
+			[entryId]: timeToString() === entryId
+				? getDailyReminderValue()
+				: null	
+		})),
+		goBack : ()=> navigation.goBack()
+	}
+}
+export default connect(mapStateToProps, mapDispatchToPrpps)(EntryDetail)
 
 
