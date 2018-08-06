a292a106a8f9889b2b22577b1ebe51d82fb94cdc
angelvinyals <angel.vinyals@gmail.com>
2018-08-06 03:07:53 +0200

    add Live Component
    undeteminated status

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, Text, ActivityIndicator,TouchableOpacity, StyleSheet } from 'react-native'
+import Foundation from 'react-native-vector-icons/Foundation'
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { white , purple } from '../utils/colors'
 
 export default class Live extends Component {
 	state= {
 		coords: null,
-		status: 'denied',
+		status: 'undetermined',
 		direction: '',
+	}
+
+	askPermission = () =>{
+
 	}
 
 	render(){
@@ -18,16 +25,30 @@ export default class Live extends Component {
 
 		if (status === 'denied') {
 			return (
-				<View>
-					<Text>Denied</Text>
+				<View style={styles.center}>
+					<Text>
+						denied
+					</Text>
 				</View>
 			)
 		}
 
 		if (status === 'undetermined') {
 			return (
-				<View>
-					<Text>undertermined</Text>
+				<View style={styles.center}>
+					<MaterialCommunityIcon
+						name='alert' 
+						color={purple}
+						size={50}
+					 />
+					<Text>
+						You need to enable location services for this app.
+					</Text>
+					<TouchableOpacity onPress={this.askPermission} style={styles.button}>
+			        	<Text style={styles.buttonText}>
+			        		Enable			        	
+			        	</Text>
+			        </TouchableOpacity>
 				</View>
 			)
 		}
@@ -40,3 +61,28 @@ export default class Live extends Component {
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText :{
+    color: white,
+    fontSize: 20,
+  }
+})
